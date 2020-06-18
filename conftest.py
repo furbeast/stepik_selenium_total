77e3a70a2d1713492a50096f6b9432842733a9b1
furbeast <slonn91@mail.ru>
2020-06-18 16:19:40 +0300

    Add login page check

--- conftest.py
+++ conftest.py
@@ -15,9 +15,6 @@ def browser(request):
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
 
-    print(user_language)
-
-
     # if browser_name == "chrome":
     #     print("\nstart chrome browser for test..")
     #     browser = webdriver.Chrome()
@@ -27,5 +24,4 @@ def browser(request):
     # else:
     #     raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,13 @@
+import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,8 +17,8 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+def test_guest_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_page()
